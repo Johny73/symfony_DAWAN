967f8d8721056decf89da12624466b0a09b5c6aa
Johny HEIJLEN <j_heijlen@hotmail.com>
2020-03-24 15:34:19 +0100

    ajout page details + liens

--- src/Controller/BoardGameController.php
+++ src/Controller/BoardGameController.php
@@ -23,4 +23,19 @@ class BoardGameController extends AbstractController
             'board_games' => $boardGames,
           ]);
     }
+    /**
+     * @Route("/{id}", requirements={"id": "\d+"})
+     */
+    public function show (int $id, BoardGameRepository $repository)
+    {
+        $boardGame = $repository->find($id);
+
+        if(!$boardGame){
+            throw $this->createNotFoundException('ce jeu n\'existe pas');
+        }
+
+        return $this->render('board_game/show.html.twig', [
+            'board_game' => $boardGame,
+        ]);
+    }
 }
