fbd32008ffd2a4a67ac104f357e9158c31d0380e
Johny HEIJLEN <j_heijlen@hotmail.com>
2020-03-26 15:16:04 +0100

    Ajout controle acces dans les routes et template

--- src/Controller/Admin/BoardGameController.php
+++ src/Controller/Admin/BoardGameController.php
@@ -7,12 +7,14 @@ namespace App\Controller\Admin;
 use App\Entity\BoardGame;
 use App\Form\BoardGameType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  *@Route("/admin/board-game")
+ * @IsGranted("ROLE_ADMIN")
  */
 class BoardGameController extends AbstractController
 {
